85319531532dba9a9296b084e33df6bb7a02ee47
Jan Funke <jan@funke.re>
2017-04-30 16:50:08 +0200

    added banner query + generic banner

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
  * @Author: Jan Funke - Tyr3al
  * @Date: 2017-04-29 22:35:54
  * @Last Modified by: Jan Funke - Tyr3al
- * @Last Modified time: 2017-04-30 11:05:50
+ * @Last Modified time: 2017-04-30 16:48:52
  */
 
 var express = require("express");
@@ -36,20 +36,51 @@ app.get("/", function (req, res) {
     res.send("API Server fully functional!");
 });
 
+app.get("/banner", function (req, res) {
+    // get all banner
+    connection.query(
+        "SELECT g.gc_code, g.name, t.name, g.owner, b.counter, b.path " +
+        "FROM geocaches g " +
+        "LEFT JOIN gc_banner b " +
+        "ON g.banner = b.id "+
+        "LEFT JOIN gc_types t " +
+        "ON g.type = t.id " +
+        "WHERE g.banner IS NOT NULL;"
+        , function (err, rows, fields) {
+        if (err) throw err;
+        res.json(rows);
+    });
+});
+
 app.get("/banner/:gcCode", function (req, res) {
     // check gc code
     var regexGC = /^GC[a-zA-Z0-9]{1,8}$/g;
-    if (req.params.gcCode != null) {
-        var matchResult = regexGC.exec(req.params.gcCode);
-        if (matchResult !== null) {
-            res.send(req.params.gcCode);
-        } else {
-            res.send("Invalid GC-Code!");
-        }
-    } else {
-        res.send("No GC-Code was submitted!");
+    var foundID = regexGC.exec(req.params.gcCode);
+    if (foundID === null) {
+        res.sendFile("./assets/banner/not_found.png", { "root": __dirname });
+        return;
     }
 
+    /* try to update counter
+    *  if 0 affected rows -> banner unknown
+    */ 
+    connection.query("UPDATE gc_banner b RIGHT JOIN geocaches g ON b.id = g.banner SET b.counter = b.counter + 1 WHERE g.gc_code = ?;", req.params.gcCode, function (err, results, fields) {
+        if (err) throw err;
+        var foundBanner = false;
+        if (results.affectedRows !== 0) {
+            var filePath = "./assets/banner/" + req.params.gcCode + ".jpg";
+            if (fs.existsSync(filePath)) {
+                foundBanner = true;
+            }
+        }
+
+        // send result
+        if (foundBanner) {
+            res.sendFile(filePath, { "root": __dirname });
+        } else {
+            res.sendFile("./assets/banner/not_found.png", { "root": __dirname });
+        }          
+    });
 });
 
 
