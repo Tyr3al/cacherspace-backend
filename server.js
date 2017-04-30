47575c725536b68921ca0c209fe047861018fbe6
Jan Funke <jan@funke.re>
2017-04-30 10:26:21 +0200

    fixed connection settings

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
  * @Author: Jan Funke - Tyr3al 
  * @Date: 2017-04-29 22:35:54 
  * @Last Modified by: Jan Funke - Tyr3al
- * @Last Modified time: 2017-04-30 09:55:46
+ * @Last Modified time: 2017-04-30 10:23:25
  */
 
 var express = require('express')
@@ -22,9 +22,16 @@ try {
 var connection = mysql.createConnection({
     host: config.dbSettings.host,
     user: config.dbSettings.user,
-    password: config.dbSettings.user,
+    password: config.dbSettings.password,
     database: config.dbSettings.database
 });
+
+try {
+    connection.connect();
+} catch (error) {
+    Console.error("Connection to database failed!");
+}
+
 
 app.get('/', function (req, res) {
     res.send('Hello World!')
